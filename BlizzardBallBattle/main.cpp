5b979d26fb0b5e8e04f70aab6670007dd67386cb
Carson Roscoe <A00925976@ad.bcit.ca>
2017-09-21 11:03:51 -0700

    Changed from cstrings to c++ string

--- BlizzardBallBattle/main.cpp
+++ BlizzardBallBattle/main.cpp
@@ -6,12 +6,13 @@
   #include <GLUT/glut.h>
   #include <GLFW/glfw3.h>
 #elif defined _WIN32 || defined _WIN64
+  #include <GL/glew.h>
   #include <SDL.h>
   #include <GL/glut.h>
   #include <GL/GL.h>
 #endif 
 #include <stdio.h>
-#include <string.h>
+#include <string>
 #include <iostream>
 #include "sprite.h"
 
