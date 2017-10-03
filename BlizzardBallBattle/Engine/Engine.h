b8e08ca03d821f46307a5aff0e514ad30e1fbf0a
Jaegar Sarauer <jaegarsarauer@jaegars-mbp.wifi.bcit.ca>
2017-10-03 14:50:38 -0700

    skeleton

--- BlizzardBallBattle/Engine/Engine.h
+++ BlizzardBallBattle/Engine/Engine.h
@@ -2,6 +2,11 @@ class Engine {
 public:
   void Update (float dt);
   void MainLoop ();
+
+  void AddListener(std::string event, std::string id, Component* component);
+  void RemoveListener(std::string event, std::string id);
+
+  void ExecuteEvent(std::string event);
  
 private:
 };
