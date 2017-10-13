3493417f6801501e85112a1858fc56bcc2d0c545
Chen <q1002608870@gmail.com>
2017-10-12 17:53:03 -0700

    fixed most problems

--- BlizzardBallBattle/Player.cpp
+++ BlizzardBallBattle/Player.cpp
@@ -2,32 +2,32 @@
 #include "transform.h"
 
 Player::Player(Shader* shader, GLuint textureBufferID) {
-	AddComponent("SpriteRenderer", new SpriteRenderer(this));
-	SpriteRenderer* renderer = (SpriteRenderer*)GetComponent("SpriteRenderer");
+	AddComponent<SpriteRenderer*>(new SpriteRenderer(this));
+	SpriteRenderer* renderer = (SpriteRenderer*)GetComponent<Transform*>();
 	renderer->SetActiveShader(shader);
 	renderer->SetActiveTexture(textureBufferID);
 }
 
 void Player::PressedDown() {
-	Transform* transform = (Transform*)GetComponent("Transform");
+	Transform* transform = (Transform*)GetComponent<Transform*>();
 	transform->addY(-0.2f);
 
 }
 
 void Player::PressedRight() {
-	Transform* transform = (Transform*)GetComponent("Transform");
+	Transform* transform = (Transform*)GetComponent<Transform*>();
 	transform->addX(0.2f);
 
 }
 
 void Player::PressedUp() {
-	Transform* transform = (Transform*)GetComponent("Transform");
+	Transform* transform = (Transform*)GetComponent<Transform*>();
 	transform->addY(0.2f);
 
 }
 
 void Player::PressedLeft() {
-	Transform* transform = (Transform*)GetComponent("Transform");
+	Transform* transform = (Transform*)GetComponent<Transform*>();
 	transform->addX(-0.2f);
 
 }
