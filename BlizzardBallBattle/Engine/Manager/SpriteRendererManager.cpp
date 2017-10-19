7225ac25ea4ab323854c14eb4a76336d4263a0a2
Carson Roscoe <A00925976@ad.bcit.ca>
2017-10-19 02:02:10 -0700

    Readded sorting layers in SpriteRendering

--- BlizzardBallBattle/Engine/Manager/SpriteRendererManager.cpp
+++ BlizzardBallBattle/Engine/Manager/SpriteRendererManager.cpp
@@ -196,15 +196,15 @@ bool SpriteRendererManager::SetOpenGLAttributes() {
     return texture;
   }
 
-  //bool SortByZ(SpriteRenderer* lhs, SpriteRenderer* rhs) {
-  //  return lhs->GetGameObject()->GetTransform()->getZ() < rhs->GetGameObject()->GetTransform()->getZ();
-  //}
+  bool SortByZ(SpriteRenderer* lhs, SpriteRenderer* rhs) {
+    return lhs->GetGameObject()->GetTransform()->getZ() < rhs->GetGameObject()->GetTransform()->getZ();
+  }
 
   void SpriteRendererManager::Render() {
     //Refresh Screen
     glClearColor(1.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
-    //std::sort(activeSprites.begin(), activeSprites.end(), SortByZ);
+    std::sort(activeSprites.begin(), activeSprites.end(), SortByZ);
     for (size_t i = 0; i < activeSprites.size(); i++) {
       SpriteRenderer* spriteRenderer = activeSprites[i];
 
