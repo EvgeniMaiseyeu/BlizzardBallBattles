4ce64daf107fa039065db96671d41dc854f881cf
Jaegar Sarauer <A00925935@ad.bcit.ca>
2017-12-09 17:57:03 -0800

    some scene fixes

--- BlizzardBallBattle/Game/Scenes/GameScene_Alpha_Networked.h
+++ BlizzardBallBattle/Game/Scenes/GameScene_Alpha_Networked.h
@@ -2,9 +2,11 @@
 
 #include "GameScene.h"
 #include "Battler.h"
+#include "Shader.h"
 
 class GameScene_Alpha_Networked : public GameScene {
 private:
+	Shader* ourShader;
 	bool isConnected;
 	void OnConnected();
 	int p1ai;
